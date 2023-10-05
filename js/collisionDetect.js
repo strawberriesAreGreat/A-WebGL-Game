56908660e130dedf882bdb9fe691fb6e12cdb3c4
strw <strw@Cassandras-MacBook-Pro.local>
2023-10-05 11:23:26 -0400

    Prettier formatting applied to all files

--- js/collisionDetect.js
+++ js/collisionDetect.js
@@ -1,56 +1,55 @@
-
-
-function isOverlapping(bacteria){
+function isOverlapping(bacteria) {
   //loop through all combinations of bacteria to check each and everyone of them against each other
   //not scalable but for the purposes of this simple game it will suffice
-  for(var i = 0; i < (bacteria.length-1) ; i++){
-    for(var j = bacteria.length-1; j > i ; j--){
-      if (calculateDistance(bacteria[i],bacteria[j])){
+  for (var i = 0; i < bacteria.length - 1; i++) {
+    for (var j = bacteria.length - 1; j > i; j--) {
+      if (calculateDistance(bacteria[i], bacteria[j])) {
         //setting larger bacteria to take over the smaller one
-        if(bacteria[i].getSize() > bacteria[j].getSize()){
-          bacteria[j].setR(bacteria[i].getR());
-          bacteria[j].setG(bacteria[i].getG());
-          bacteria[j].setB(bacteria[i].getB());
-        }else{
-          bacteria[i].setR(bacteria[j].getR());
-          bacteria[i].setG(bacteria[j].getG());
-          bacteria[i].setB(bacteria[j].getB());
+        if (bacteria[i].getSize() > bacteria[j].getSize()) {
+          bacteria[j].setR(bacteria[i].getR())
+          bacteria[j].setG(bacteria[i].getG())
+          bacteria[j].setB(bacteria[i].getB())
+        } else {
+          bacteria[i].setR(bacteria[j].getR())
+          bacteria[i].setG(bacteria[j].getG())
+          bacteria[i].setB(bacteria[j].getB())
         }
-
       }
-    };
-  };
+    }
+  }
 }
-function isPoisoning(antiBacteria,bacteria){
+function isPoisoning(antiBacteria, bacteria) {
   //loop through all combinations of bacteria to check each and everyone of them against each other
   //not scalable but for the purposes of this simple game it will suffice
-  for(var i = 0; i < antiBacteria.length ; i++){
-    for(var j = 0; j < bacteria.length ; j++){
-      if (calculateDistance(antiBacteria[i],bacteria[j])){
+  for (var i = 0; i < antiBacteria.length; i++) {
+    for (var j = 0; j < bacteria.length; j++) {
+      if (calculateDistance(antiBacteria[i], bacteria[j])) {
         //deleting bacteria if and when it hits antibacteria
-        console.log(" Antibiotic has killed a bacteria colony ");
-        game.deleteBacteria(j);
-        document.getElementById("score2").innerHTML = writeScore();
+        console.log(' Antibiotic has killed a bacteria colony ')
+        game.deleteBacteria(j)
+        document.getElementById('score2').innerHTML = writeScore()
       }
-    };
-  };
+    }
+  }
 }
 
 //collision detection for the bacteria and soon to be anti-biotics
 function calculateDistance(particleA, particleB) {
+  orginAx = particleA.getOrginX()
+  orginAy = particleA.getOrginY()
+  orginBx = particleB.getOrginX()
+  orginBy = particleB.getOrginY()
 
-    orginAx = particleA.getOrginX();
-    orginAy = particleA.getOrginY();
-    orginBx = particleB.getOrginX();
-    orginBy = particleB.getOrginY();
+  radiusA = particleA.getSize()
+  radiusB = particleB.getSize()
 
-    radiusA = particleA.getSize();
-    radiusB = particleB.getSize();
-
-    // distance between their orgins = a^2 + b^2 (pythagorean theorem)
-    // since the distance is just the hypotenuse
-    var orginDistance = Math.sqrt((orginAx-orginBx)*(orginAx-orginBx) + (orginAy-orginBy)*(orginAy-orginBy));
-    var combinedRadiuses = radiusA + radiusB;
-    // if the combine radius > distance between orgins we know they overlap so we return true
-    return (combinedRadiuses >= orginDistance);
+  // distance between their orgins = a^2 + b^2 (pythagorean theorem)
+  // since the distance is just the hypotenuse
+  var orginDistance = Math.sqrt(
+    (orginAx - orginBx) * (orginAx - orginBx) +
+      (orginAy - orginBy) * (orginAy - orginBy),
+  )
+  var combinedRadiuses = radiusA + radiusB
+  // if the combine radius > distance between orgins we know they overlap so we return true
+  return combinedRadiuses >= orginDistance
 }
--- js/main.js
+++ js/main.js
@@ -1,73 +1,68 @@
-var game;
+var game
 
 function main() {
   game = window.game
 
   //checking if we've selected a bacteria to kill
-  var canvas = document.querySelector('canvas');
-  var gl = canvas.getContext('webgl');
-  var pixels = new Uint8Array(4);
+  var canvas = document.querySelector('canvas')
+  var gl = canvas.getContext('webgl')
+  var pixels = new Uint8Array(4)
 
   //tracking where the last click took place
   var elem = document.getElementById('canvas'),
     eLeft = elem.offsetLeft + elem.clientLeft,
     eTop = elem.offsetTop + elem.clientTop,
     c = elem.getContext('2d'),
-    elements = [];
+    elements = []
 
-  canvas.addEventListener("click", function(event) {
-
-    var x = event.clientX - eLeft;
-    var y = 800 - (event.clientY - eTop);
-    console.log(event.clientX + " " + event.clientY);
+  canvas.addEventListener('click', function (event) {
+    var x = event.clientX - eLeft
+    var y = 800 - (event.clientY - eTop)
+    console.log(event.clientX + ' ' + event.clientY)
 
     //now we need to request animation frame because the canvas is in a constant state of flux as it renders
-    requestAnimationFrame(function() {
+    requestAnimationFrame(function () {
       //finally getting the pixel values of the spot clicked
-      gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+      gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
       //checking if the bacteria has been clicked on (any pixel with an R value != 0 or 255 (black or white))
       if (pixels[0] != 255 && pixels[0] != 0) {
-        game.updateBacteria(pixels);
+        game.updateBacteria(pixels)
       }
-      game.updateAntiBacteria(x,y);
-    });
-  });
+      game.updateAntiBacteria(x, y)
+    })
+  })
 }
 
 //updates the game as frames are updated
 function loop() {
-  isOverlapping(game.getBacteria());
-  isPoisoning(game.getAntiBacteria(),game.getBacteria());
-  window.game.update();
+  isOverlapping(game.getBacteria())
+  isPoisoning(game.getAntiBacteria(), game.getBacteria())
+  window.game.update()
   if (game.hasWon()) {
-    document.getElementById("score1").innerHTML = "YOU WIN!";
-    return;
+    document.getElementById('score1').innerHTML = 'YOU WIN!'
+    return
   }
   if (game.hasLost()) {
-    alert("GAME OVER, YOU LOSE!");
-    document.getElementById("score1").innerHTML = "YOU LOSE!";
-    return;
+    alert('GAME OVER, YOU LOSE!')
+    document.getElementById('score1').innerHTML = 'YOU LOSE!'
+    return
   }
-  requestAnimationFrame(loop);
+  requestAnimationFrame(loop)
 }
 
 function writeScore() {
-  return (game.getScore() + "/" + game.getPoints());
+  return game.getScore() + '/' + game.getPoints()
 }
 
 function areSameColor(r1, g1, b1, r2, g2, b2) {
-
-  let dR = Math.abs(r1 - r2);
-  let dG = Math.abs(g1 - g2);
-  let dB = Math.abs(b1 - b2);
-  let diff = dR + dG + dB;
+  let dR = Math.abs(r1 - r2)
+  let dG = Math.abs(g1 - g2)
+  let dB = Math.abs(b1 - b2)
+  let diff = dR + dG + dB
 
   if (diff < 4) {
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
-
-
-
 }
